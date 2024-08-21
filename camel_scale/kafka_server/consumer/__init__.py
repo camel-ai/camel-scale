a64aea98fc8f9cdc0bd287d7b40b83042fa57c7b
Zikang CHEN <65004114+Appointat@users.noreply.github.com>
2024-08-21 16:10:08 +0800

    Refactor Kafka consumer and producer modules to use Confluent Kafka library

--- camel_scale/kafka_server/consumer/__init__.py
+++ camel_scale/kafka_server/consumer/__init__.py
@@ -12,8 +12,9 @@
 # limitations under the License.
 # =========== Copyright 2023 @ CAMEL-AI.org. All Rights Reserved. ===========
 
-from .consumer import Consumer
+from .consumer import ConfluentConsumer, Consumer
 
 __all__ = [
     "Consumer",
+    "ConfluentConsumer",
 ]
--- camel_scale/kafka_server/producer/__init__.py
+++ camel_scale/kafka_server/producer/__init__.py
@@ -12,8 +12,9 @@
 # limitations under the License.
 # =========== Copyright 2023 @ CAMEL-AI.org. All Rights Reserved. ===========
 
-from .producer import Producer
+from .producer import ConfluentProducer, Producer
 
 __all__ = [
     "Producer",
+    "ConfluentProducer",
 ]
